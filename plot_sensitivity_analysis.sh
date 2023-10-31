268b8e51bd99b807a0be3580de806d3e5d404231
am-kaiser <63399571+am-kaiser@users.noreply.github.com>
2023-10-31 11:37:57 +0100

    added new files

---
+++ plot_sensitivity_analysis.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH --account=ec158
+
+# Runtime and memory
+#SBATCH --mem=100GB
+#SBATCH --time=1-00:00:00
+
+#SBATCH --ntasks-per-node=1 # number of cores, max 128 on fox
+
+singularity exec -H /fp/projects01/ec158/abl_scm_perturbation_study/ docker/abl_scm_venv.sif python3 -u -m single_column_model.post_processing.perform_sensitivity_analysis
