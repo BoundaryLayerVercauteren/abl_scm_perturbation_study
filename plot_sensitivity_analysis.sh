c3fc0cbe20df73fa7322f742f3da1ddc386adb7b
am-kaiser <63399571+am-kaiser@users.noreply.github.com>
2023-10-31 11:59:19 +0100

    updated

--- plot_sensitivity_analysis.sh
+++ plot_sensitivity_analysis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --account=ec158
+#SBATCH --account=account_name
 
 # Runtime and memory
 #SBATCH --mem=100GB
@@ -8,4 +8,4 @@
 
 #SBATCH --ntasks-per-node=1 # number of cores, max 128 on fox
 
-singularity exec -H /fp/projects01/ec158/abl_scm_perturbation_study/ docker/abl_scm_venv.sif python3 -u -m single_column_model.post_processing.perform_sensitivity_analysis
+singularity exec -H /fp/projects01/account_name/abl_scm_perturbation_study/ docker/abl_scm_venv.sif python3 -u -m single_column_model.post_processing.perform_sensitivity_analysis
