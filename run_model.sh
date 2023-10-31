4d449397458ffe144be25b8b1bf3dff7363acac0
am-kaiser <63399571+am-kaiser@users.noreply.github.com>
2023-10-31 11:41:02 +0100

    test filter

--- run_model.sh
+++ run_model.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --account=ec158
+#SBATCH --account=account_name
 
 # Runtime and memory
 #SBATCH --mem=100GB
-#SBATCH --time=1-00:00:00
+#SBATCH --time=10:00:00
 
 #SBATCH --ntasks-per-node=1 # number of cores, max 128 on fox
 
-singularity exec -H /fp/projects01/ec158/abl_scm_perturbation_study/ docker/abl_scm_venv.sif python3 -u main.py
+singularity exec -H /fp/projects01/account_name/abl_scm_perturbation_study/ docker/abl_scm_venv.sif python3 -u main.py
