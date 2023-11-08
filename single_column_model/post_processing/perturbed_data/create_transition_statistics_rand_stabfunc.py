7d6307d9c85b83ee9652bd6a9233337fb669a820
am-kaiser <63399571+am-kaiser@users.noreply.github.com>
2023-11-08 13:52:41 +0100

    new scripts to study sensitivty for runs with random stab func

---
+++ single_column_model/post_processing/perturbed_data/create_transition_statistics_rand_stabfunc.py
@@ -0,0 +1,68 @@
+import json
+import os
+import random
+from concurrent.futures import ProcessPoolExecutor
+import h5py
+import numpy as np
+
+# ---------------------------------------------------------------------------
+# Define directory where simulation output is saved
+output_directory = "output/sensitivity_study/internal_variability/very_weakly/"
+# ---------------------------------------------------------------------------
+# Define where the unstable equilibrium is located
+location_unstable_eq = 5
+# ---------------------------------------------------------------------------
+def check_if_transitioned(values):
+    if values[0] > location_unstable_eq:
+        if any(values < location_unstable_eq):
+            return 1
+        else:
+            return 0
+    else:
+        if any(values > location_unstable_eq):
+            return 1
+        else:
+            return 0
+
+
+def combine_info_about_simulation_type(file_path):
+    try:
+        with h5py.File(file_path, "r+") as file:
+            z = file["z"][:]
+            z_idx = (np.abs(z - 20)).argmin()
+            theta = file["theta"][:]
+            delta_theta = theta[z_idx, :] - theta[0, :]
+    except OSError:
+        return
+
+    # Extract u value for simulation from file path
+    file_name = np.array(file_path.split("/"))[-1]
+    file_elements = np.array(file_name.split("_"))
+
+    u_val = float(file_elements[2])
+    sigma_s = float(file_elements[4])
+    sim_idx = int(file_elements[-1][:-3])
+
+    # Calculate how many transitions take place on average over all simulations
+    transitioned = check_if_transitioned(delta_theta.flatten())
+
+    # Save parameter combination for this simulation
+    return (u_val, sigma_s, sim_idx, transitioned)
+
+
+# Get all solution files
+output_files = []
+for path, subdirs, files in os.walk(output_directory):
+    for name in files:
+        output_files.append(os.path.join(path, name))
+
+parameter_comb = []
+
+with ProcessPoolExecutor(max_workers=125) as executor:
+    for result in executor.map(combine_info_about_simulation_type, output_files):
+        if result:
+            parameter_comb.append(result)
+
+# Save calculated values in file
+with open(f"{output_directory}transition_overview.json", "w") as file:
+    json.dump(parameter_comb, file)
---
+++ single_column_model/post_processing/perturbed_data/visualize_sensitivity_analysis_stabfunc.py
@@ -0,0 +1,93 @@
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+from operator import itemgetter
+
+# Set plotting style
+plt.style.use("science")
+
+# Set font sizes for plots
+SMALL_SIZE = 16
+MEDIUM_SIZE = 18
+BIGGER_SIZE = 20
+
+plt.rc("font", size=SMALL_SIZE)  # controls default text sizes
+plt.rc("axes", titlesize=SMALL_SIZE)  # fontsize of the axes title
+plt.rc("axes", labelsize=MEDIUM_SIZE)  # fontsize of the x and y labels
+plt.rc("xtick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+plt.rc("ytick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+plt.rc("legend", fontsize=SMALL_SIZE)  # legend fontsize
+plt.rc("figure", titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+# Define directory where simulation output is saved
+output_directory = "output/sensitivity_study/u_and_model/"  # u_and_model/'  #internal_variability
+trans_percentage = 0.8
+
+# Load results from sensitivity analysis
+with open(f"{output_directory}transition_overview.json") as file:
+    result = json.load(file)
+
+
+# Sort order by wind velocity value and then sigma(s)
+def sort_list_of_list(input):
+    input.sort(key=itemgetter(2))
+    input.sort(key=itemgetter(1))
+    input.sort(key=itemgetter(0))
+    return input
+
+
+result = sort_list_of_list(result)
+
+uG_range = np.unique([elem[0] for elem in result])
+sigma_s_range = np.unique([elem[1] for elem in result])
+sim_idx_range = np.unique([elem[2] for elem in result])
+
+
+def average_num_transition_over_all_sim(statistics, uG, sigma_s, sim_idx):
+    trans_sum = [(param_comb, sum(x[3] for x in statistics if (x[0] == param_comb[0] and x[1] == param_comb[1]))) for
+                 param_comb in zip(uG, sigma_s)]
+
+    for idx, elem in enumerate(trans_sum):
+        trans_sum[idx][2] = trans_sum[idx][2]/np.max(sim_idx)*100
+
+    return trans_sum
+
+
+def get_minimal_sigma_with_trans_for_every_u(trans_statistics, u_range):
+    first_sigma_with_enough_trans = []
+    for u in u_range:
+        # Find indices of the parameters which correspond to the current u
+        cor_idx = np.where([cur_u[0] for cur_u in trans_statistics] == u)[0]
+        # Find out how many transitions (on average) took place for the simulations corresponding to the current u
+        cor_average_num_trans = np.array([trans_statistics[i][2] for i in cor_idx])
+        idx_first_sigma_with_enough_trans = cor_idx[np.argmax(cor_average_num_trans >= trans_percentage)]
+        if trans_statistics[idx_first_sigma_with_enough_trans][2]< trans_percentage:
+            first_sigma_with_enough_trans.append(np.nan)
+        else:
+            first_sigma_with_enough_trans.append(trans_statistics[idx_first_sigma_with_enough_trans][1])
+
+    return first_sigma_with_enough_trans
+
+
+min_sigma = np.array(get_minimal_sigma_with_trans_for_every_u(result, uG_range))
+
+fig, ax = plt.subplots(1, 1, figsize=(10, 5))
+
+#ax.axvspan(5.31, 5.89, alpha=0.3, color="red", label="bistable region")
+
+ax.plot(uG_range, min_sigma[:, 0], color='darkgreen')
+ax.scatter(uG_range, min_sigma[:, 0], color='darkgreen', marker="v")
+
+#ax.set_xlim((4.5, 6.9))
+#ax.yaxis.set_major_locator(plt.MultipleLocator(0.04))
+#ax.xaxis.set_major_locator(plt.MultipleLocator(0.2))
+#ax.set_axisbelow(True)
+ax.grid()
+
+fig.legend(facecolor="white", edgecolor="black", frameon=True, bbox_to_anchor=(1.0, 0.95), loc='upper left')
+
+ax.set_xlabel(r"U [$\mathrm{ms^{-1}}$]")
+ax.set_ylabel(r"$\sigma_{s,min} [\mathrm{?}]$")
+
+fig.tight_layout()
+plt.savefig(f"{output_directory}transition_statistics.pdf", bbox_inches="tight", dpi=300)
