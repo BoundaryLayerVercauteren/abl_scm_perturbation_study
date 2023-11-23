f2ac1da8ede26f00d3517eba45af2d4d5aaf2075
am-kaiser <63399571+am-kaiser@users.noreply.github.com>
2023-11-23 14:28:31 +0100

    remove lines for testing

--- single_column_model/model/run_model.py
+++ single_column_model/model/run_model.py
@@ -170,16 +170,13 @@ def setup_for_sensitivity_study(parameters):
                                  ).T.reshape(-1, 7)
 
     # Remove all rows where sim_idx>0 and perturbation strength=0
-    print('1', np.shape(param_combination))
     if len(sim_idx_list) > 1:
         rows_to_be_deleted = []
         for row_idx in np.arange(0, np.shape(param_combination)[0]):
             if param_combination[row_idx, -2] == 0 and param_combination[row_idx, -1] > 0:
                 rows_to_be_deleted.append(row_idx)
         param_combination = np.delete(param_combination, rows_to_be_deleted, 0)
-        print(rows_to_be_deleted)
-        print('2', np.shape(param_combination))
-    exit()
+
     return param_combination
 
 
