05d26c4e8fa28f0f0b4efa059efdebe3bc4a9d69
am-kaiser <63399571+am-kaiser@users.noreply.github.com>
2024-01-04 13:59:55 +0100

    moved plotting style to sepparate file

---
+++ single_column_model/post_processing/set_plotting_style.py
@@ -0,0 +1,19 @@
+import scienceplots
+import matplotlib.pyplot as plt
+
+def set_style_of_plots(figsize):
+    plt.style.use("science")
+
+    # Set font sizes for plots
+    if figsize == (10,5):
+        SMALL_SIZE = 11
+        MEDIUM_SIZE = 12
+        BIGGER_SIZE = 15
+
+    plt.rc("font", size=SMALL_SIZE)  # controls default text sizes
+    plt.rc("axes", titlesize=SMALL_SIZE)  # fontsize of the axes title
+    plt.rc("axes", labelsize=MEDIUM_SIZE)  # fontsize of the x and y labels
+    plt.rc("xtick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+    plt.rc("ytick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+    plt.rc("legend", fontsize=SMALL_SIZE)  # legend fontsize
+    plt.rc("figure", titlesize=BIGGER_SIZE)  # fontsize of the figure title
