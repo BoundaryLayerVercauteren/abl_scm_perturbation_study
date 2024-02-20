ecc78de5e81c296401a675e2a372a35f66749e41
am-kaiser <63399571+am-kaiser@users.noreply.github.com>
2024-02-20 12:54:59 +0100

    reformatted

--- single_column_model/model/run_model.py
+++ single_column_model/model/run_model.py
@@ -3,18 +3,21 @@ import numpy as np
 import sys
 from functools import partial
 
+# Project related imports
+from single_column_model.model import space_discretization  # define_stochastic_part,
+from single_column_model.model import (
+    define_initial_and_boundary_conditions,
+    define_PDE_model,
+    define_perturbation,
+    define_parts_for_stoch_stability_function,
+    solve_PDE_model,
+)
+from single_column_model.utils import save_solution
+
 if "pytest" in sys.modules:
     from single_column_model.tests import parameters
 else:
     from single_column_model.model import parameters
-
-# Project related imports
-from single_column_model.model import \
-    space_discretization  # define_stochastic_part,
-from single_column_model.model import (define_initial_and_boundary_conditions,
-                                       define_PDE_model, define_perturbation,
-                                       define_parts_for_stoch_stability_function, solve_PDE_model)
-from single_column_model.utils import save_solution
 
 
 def make_setup_for_model_run():
@@ -22,11 +25,13 @@ def make_setup_for_model_run():
     params, fparams, output = parameters.initialize_project_variables()
 
     # To make naming of output files correct
-    if params.perturbation_type == 'none' and params.perturbation_param != 'stab_func':
-        params.perturbation_strength = 'nan'
+    if params.perturbation_type == "none" and params.perturbation_param != "stab_func":
+        params.perturbation_strength = "nan"
 
     # Create directory for solutions and initial conditions (if required)
-    output.solution_directory, output.init_directory = save_solution.create_solution_directory(params)
+    output.solution_directory, output.init_directory = save_solution.create_solution_directory(
+        params
+    )
     output.top_solution_directory = output.solution_directory
 
     return params, fparams, output
@@ -38,22 +43,31 @@ def combine_model_solver_functions(fenics_params, params, output):
 
     # Create stochastic grid
     params.stoch_grid, params.Hs_det_idx, params.Hs, params.Ns_n, params.dz_s = define_parts_for_stoch_stability_function.make_stochastic_grid(
-        params, mesh.coordinates())
+        params, mesh.coordinates()
+    )
 
     # Define variables to use the fenics library
     fenics_params = define_PDE_model.setup_fenics_variables(fenics_params, mesh)
 
     # Define boundary conditions
-    fenics_params, params = define_initial_and_boundary_conditions.define_boundary_conditions(fenics_params, params)
+    fenics_params, params = define_initial_and_boundary_conditions.define_boundary_conditions(
+        fenics_params, params
+    )
 
     # Define initial profiles/ values
-    u_n, v_n, T_n, k_n = define_initial_and_boundary_conditions.define_initial_conditions(fenics_params.Q, mesh, params)
+    u_n, v_n, T_n, k_n = define_initial_and_boundary_conditions.define_initial_conditions(
+        fenics_params.Q, mesh, params
+    )
 
     # Set up the weak formulation of the equations
-    fenics_params.F = define_PDE_model.weak_formulation(fenics_params, params, u_n, v_n, T_n, k_n)
+    fenics_params.F = define_PDE_model.weak_formulation(
+        fenics_params, params, u_n, v_n, T_n, k_n
+    )
 
     # Set up the stochastic solver
-    stoch_solver, params = define_parts_for_stoch_stability_function.initialize_SDEsolver(params)
+    stoch_solver, params = define_parts_for_stoch_stability_function.initialize_SDEsolver(
+        params
+    )
 
     # Create the variables to write output
     output = save_solution.initialize(output, params)
@@ -62,7 +76,9 @@ def combine_model_solver_functions(fenics_params, params, output):
     output = define_perturbation.create_perturbation(params, fenics_params, output)
 
     # Solve the system
-    output = solve_PDE_model.solution_loop(params, output, fenics_params, stoch_solver, u_n, v_n, T_n, k_n)
+    output = solve_PDE_model.solution_loop(
+        params, output, fenics_params, stoch_solver, u_n, v_n, T_n, k_n
+    )
 
     # plot_solution.make_3d_plot(output, params, fenics_params)
 
@@ -100,23 +116,24 @@ def setup_simulation_parameters(input_val, parameter_class_val):
     return parameter_class_val
 
 
-def run_single_simulation_model(
-        model_param,
-        fenics_param,
-        output_val,
-        sim_parameters,
-):
+def run_single_simulation_model(model_param, fenics_param, output_val, sim_parameters):
     """Function to perform one single model run for a given set of parameters."""
     # Update parameter
     model_param = setup_simulation_parameters(sim_parameters, model_param)
 
     # Set random seed dependent on sim_idx as otherwise the same one will be used for all processes on the same core
-    seed_val = int(model_param.sim_index+model_param.u_G*100+model_param.perturbation_strength*100)
+    seed_val = int(
+        model_param.sim_index
+        + model_param.u_G * 100
+        + model_param.perturbation_strength * 100
+    )
 
     np.random.seed(seed_val)
 
     # Define file name for initial conditions
-    model_param.init_path = f'{output_val.init_directory}{model_param.init_cond_path}Ug{model_param.u_G}'
+    model_param.init_path = (
+        f"{output_val.init_directory}{model_param.init_cond_path}Ug{model_param.u_G}"
+    )
 
     # Creat subdirectory for current simulation type
     output_val = save_solution.create_sub_solution_directory(model_param, output_val)
@@ -142,41 +159,56 @@ def run_single_simulation_model(
 
 
 def setup_for_sensitivity_study(parameters):
-    if parameters.perturbation_param == 'u and theta' or parameters.perturbation_param == 'theta and u':
-        perturbation_param_list = ['u', 'theta']
+    if (
+        parameters.perturbation_param == "u and theta"
+        or parameters.perturbation_param == "theta and u"
+    ):
+        perturbation_param_list = ["u", "theta"]
     else:
         perturbation_param_list = [parameters.perturbation_param]
 
-    if parameters.perturbation_type == 'pos and neg' or parameters.perturbation_type == 'neg and pos':
-        perturbation_type_list = ['pos', 'neg']
+    if (
+        parameters.perturbation_type == "pos and neg"
+        or parameters.perturbation_type == "neg and pos"
+    ):
+        perturbation_type_list = ["pos", "neg"]
     else:
         perturbation_type_list = [parameters.perturbation_type]
 
-    if parameters.perturbation_time_spread == 'all':
+    if parameters.perturbation_time_spread == "all":
         perturbation_time_spread_list = np.arange(100, 600, 100)
     else:
         perturbation_time_spread_list = [parameters.perturbation_time_spread]
 
-    if parameters.perturbation_height_spread == 'all':
+    if parameters.perturbation_height_spread == "all":
         perturbation_height_spread_list = np.array([1, 5, 10])
     else:
         perturbation_height_spread_list = [parameters.perturbation_height_spread]
 
     u_G_list = np.round(parameters.u_G_range, 1)
 
-    perturb_strength_list = np.round(np.arange(0, parameters.perturbation_max + parameters.perturbation_step_size,
-                                               parameters.perturbation_step_size), 4)
+    perturb_strength_list = np.round(
+        np.arange(
+            0,
+            parameters.perturbation_max + parameters.perturbation_step_size,
+            parameters.perturbation_step_size,
+        ),
+        4,
+    )
 
     sim_idx_list = np.arange(0, parameters.num_simulation).astype(int)
 
-    param_combination = np.array(np.meshgrid(perturbation_param_list,
-                                             perturbation_type_list,
-                                             perturbation_time_spread_list,
-                                             perturbation_height_spread_list,
-                                             u_G_list,
-                                             perturb_strength_list,
-                                             sim_idx_list)
-                                 ).T.reshape(-1, 7)
+    param_combination = np.array(
+        np.meshgrid(
+            perturbation_param_list,
+            perturbation_type_list,
+            perturbation_time_spread_list,
+            perturbation_height_spread_list,
+            u_G_list,
+            perturb_strength_list,
+            sim_idx_list,
+        )
+    ).T.reshape(-1, 7)
 
     # # Remove all rows where sim_idx>0 and perturbation strength=0
     # if len(sim_idx_list) > 1:
@@ -194,12 +226,19 @@ def split_into_job_array_tasks(param_comb):
         job_idx = int(sys.argv[1]) - 1
         total_num_jobs = int(sys.argv[2])
 
-        task_indices = np.linspace(0, np.shape(param_comb)[0], total_num_jobs + 1).astype(int)
+        task_indices = np.linspace(
+            0, np.shape(param_comb)[0], total_num_jobs + 1
+        ).astype(int)
         if task_indices[-1] != np.shape(param_comb)[0]:
             task_indices = np.append(task_indices, np.shape(param_comb)[0])
-        task_start_end_idx = [(task_indices[i], task_indices[i + 1]) for i in np.arange(0, len(task_indices[:-1]))]
+        task_start_end_idx = [
+            (task_indices[i], task_indices[i + 1])
+            for i in np.arange(0, len(task_indices[:-1]))
+        ]
 
-        return param_comb[task_start_end_idx[job_idx][0]:task_start_end_idx[job_idx][1], :]
+        return param_comb[
+            task_start_end_idx[job_idx][0] : task_start_end_idx[job_idx][1], :
+        ]
     else:
         return param_comb
 
@@ -207,9 +246,11 @@ def split_into_job_array_tasks(param_comb):
 def run_sensitivity_study(in_params, fen_params, out_params):
     """Function to run model for a combination of parameters in parallel."""
     # Make sure that all required parameter are given
-    if in_params.perturbation_type == 'none' or in_params.perturbation_param == 'none':
-        sys.exit("The type of perturbation and to which equation it should be added needs to be specified to run the "
-                 "sensitivity analysis.")
+    if in_params.perturbation_type == "none" or in_params.perturbation_param == "none":
+        sys.exit(
+            "The type of perturbation and to which equation it should be added needs to be specified to run the "
+            "sensitivity analysis."
+        )
     # Define range of parameters
     unique_param_combinations = setup_for_sensitivity_study(in_params)
 
@@ -219,13 +260,19 @@ def run_sensitivity_study(in_params, fen_params, out_params):
 
     # Solve model for every parameter combination
     with multiprocessing.Pool(processes=in_params.num_proc) as pool:
-        pool.map(partial(run_single_simulation_model, in_params, fen_params, out_params), sub_param_combinations)
+        pool.map(
+            partial(run_single_simulation_model, in_params, fen_params, out_params),
+            sub_param_combinations,
+        )
 
 
 def run_multi_uG_simulations(in_params, fen_params, out_params):
     """Function to run model for a several geostrophic wind in parallel."""
     with multiprocessing.Pool(processes=in_params.num_proc) as pool:
-        pool.map(partial(run_single_simulation_model, in_params, fen_params, out_params), in_params.u_G_range)
+        pool.map(
+            partial(run_single_simulation_model, in_params, fen_params, out_params),
+            in_params.u_G_range,
+        )
 
 
 def run_model():
